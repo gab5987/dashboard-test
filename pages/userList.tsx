4976fad41e496b2aa8451335d9bafa9ad75471b3
gab5987 <biell.gb4@icloud.com>
2022-12-10 17:29:34 -0300

    Fix: Vercel not deploying page

--- pages/userList.tsx
+++ pages/userList.tsx
@@ -1,50 +1,7 @@
 import React from "react";
 
-const FlotingThings = () => {
-    return (
-        <div className="mt-4">
-            <div className="flex flex-wrap -mx-6">
-                <div className=" px-6 sm:w-1/2 xl:w-1/2">
-                    <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-gray-900">
-                        <div className="p-3 rounded-full bg-gray-500 bg-opacity-75">
-                            <svg className="h-8 w-8 text-gray-200" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z" fill="currentColor"/>
-                                <path d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z" fill="currentColor"/>
-                                <path d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z" fill="currentColor"/>
-                                <path d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z" fill="currentColor"/>
-                                <path d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z" fill="currentColor"/>
-                                <path d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z" fill="currentColor"/>
-                            </svg>
-                        </div>
-
-                        <div className="mx-5">
-                            <h4 className="text-2xl font-semibold text-gray-200">8,282</h4>
-                            <div className="text-gray-300">Novos Usuarios</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="w-full mt-6 px-6 sm:w-1/2 xl:w-1/2 xl:mt-0">
-                    <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-gray-900">
-                        <div className="p-3 rounded-full bg-gray-500 bg-opacity-75">
-                            <svg className="h-8 w-8 text-gray-200" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
-                            </svg>
-                        </div>
-
-                        <div className="mx-5">
-                            <h4 className="text-2xl font-semibold text-gray-200">136,486 TPS</h4>
-                            <div className="text-gray-300">Trafego da API</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
 class UserList extends React.Component {
-    componentWillMount(): void {
+    UNSAFE_componentWillMount(): void {
         CreateUsers();
     }
     render() { console.log(Content)
@@ -128,4 +85,47 @@ function User(i: number) {
             </td>
         </tr>
     )
-} 
+}
+
+const FlotingThings = () => {
+    return (
+        <div className="mt-4">
+            <div className="flex flex-wrap -mx-6">
+                <div className=" px-6 sm:w-1/2 xl:w-1/2">
+                    <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-gray-900">
+                        <div className="p-3 rounded-full bg-gray-500 bg-opacity-75">
+                            <svg className="h-8 w-8 text-gray-200" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z" fill="currentColor"/>
+                                <path d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z" fill="currentColor"/>
+                                <path d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z" fill="currentColor"/>
+                                <path d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z" fill="currentColor"/>
+                                <path d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z" fill="currentColor"/>
+                                <path d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z" fill="currentColor"/>
+                            </svg>
+                        </div>
+
+                        <div className="mx-5">
+                            <h4 className="text-2xl font-semibold text-gray-200">8,282</h4>
+                            <div className="text-gray-300">Novos Usuarios</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full mt-6 px-6 sm:w-1/2 xl:w-1/2 xl:mt-0">
+                    <div className="flex items-center px-5 py-6 shadow-sm rounded-md bg-gray-900">
+                        <div className="p-3 rounded-full bg-gray-500 bg-opacity-75">
+                            <svg className="h-8 w-8 text-gray-200" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+
+                        <div className="mx-5">
+                            <h4 className="text-2xl font-semibold text-gray-200">136,486 TPS</h4>
+                            <div className="text-gray-300">Trafego da API</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
